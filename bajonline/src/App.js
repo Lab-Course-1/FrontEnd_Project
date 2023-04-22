36e4d6295882282a251c2fa748b27e74fef92619
unknown <blendikras96@gmail.com>
2023-04-22 15:11:24 +0200

    Added Login/Register Page

--- bajonline/src/App.js
+++ bajonline/src/App.js
@@ -4,6 +4,8 @@ import Home from "./Pages/Home/Home"
 import Contact from './Pages/Contact/Contact'
 import OurStory from './Pages/OurStory/OurStory'
 import Oops from './Pages/Components/Oops/Oops'
+import Login from './Pages/Login/Login'
+import Register from './Pages/Register/Register'
 
 
 
@@ -14,6 +16,8 @@ function App() {
         <Route path="/" element={<Home />} exact />
         <Route path="/shopall" element={<ShopAll />} exact />
         <Route path="/contact" element={<Contact />} exact />
+        <Route path='/login' element={<Login/>} exact />
+        <Route path='/register' element={<Register/>} exact />
         <Route path="/ourstory" element={<OurStory />} exact />
         <Route path="*" element={<Oops />}/>
       </Routes>
