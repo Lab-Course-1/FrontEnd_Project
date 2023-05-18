74fdb9604307264274844ea104d5e9e0ab67e423
blendikrass <blendikras96@gmail.com>
2023-05-18 21:35:08 +0200

    Created Layout

--- bajonline/src/Pages/ProductPage/ProductPage.js
+++ bajonline/src/Pages/ProductPage/ProductPage.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import Navbar from "../Components/Navbar/Navbar"
+import Footer from "../Components/Footer/Footer"
+
 
 const ProductPage = () => {
-  return (
-    <div>ProductPage</div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <div className='product__page'>ProductPage
+                <div className='left__part'>
+
+                </div>
+                
+                <div className='mid__part'>
+
+                </div>
+
+                <div className='right__part'>
+
+                </div>
+
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
-export default ProductPage
+export default ProductPage  
