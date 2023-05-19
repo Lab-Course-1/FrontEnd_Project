685a92530ea831e50d5943764be3190e3076b1db
blendikrass <blendikras96@gmail.com>
2023-05-19 16:05:25 +0200

    Color Division

--- bajonline/src/Pages/ProductPage/ProductPage.js
+++ bajonline/src/Pages/ProductPage/ProductPage.js
@@ -38,7 +38,16 @@ const ProductPage = () => {
                 <div className='right__part'>
                     <div className='product__name'>
                         <h1 className='product__title'>Name Of Product</h1>
-                        <p className='product__description'>Descriptio of product: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <p className='product__description'>Description of product: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <hr className='section__divider' />
+                    <div className='color__selection'>
+                        <p className='button'>colors</p>
+                        <ul className='color__list'>
+                            <li className='color__item red'>Red</li>
+                            <li className='color__item green'>Green</li>
+                            <li className='color__item brown'>Brown</li>
+                        </ul>
                     </div>
                 </div>
             </div>
