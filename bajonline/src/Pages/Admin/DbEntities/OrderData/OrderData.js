b887f99b3dc6efc24f5783ad665fed195076dc76
albion599 <albionpaqarizi1@gmail.com>
2023-06-10 20:33:34 +0200

    Added OrderData page in Administration

--- bajonline/src/Pages/Admin/DbEntities/OrderData/OrderData.js
+++ bajonline/src/Pages/Admin/DbEntities/OrderData/OrderData.js
@@ -8,37 +8,26 @@ const OrderData = () => {
       orderId: '1',
       orderDate: '2023-06-01',
       shippingDate: '2023-06-05',
-      orderPrice: 100.0,
       orderFinalPrice: 90.0,
-      trackingId: 'ABC123',
-      carrier: 'UPS',
-      orderStatus: 'Shipped',
       paymentStatus: 'Paid',
-      transactionId: 'XYZ789',
-      paymentMethodId: 'CreditCard',
-      paymentDate: '2023-06-02',
-      paymentDueDate: '2023-06-07',
-      phoneNumber: '049123456',
-      streetAddress: '123 Main St',
-      city: 'Malisheve',
-      country: 'Kosove',
-      postalCode: '24000',
-      name: 'Albion Paqarizi',
       userId: '2',
-      user: {
-        id: '2',
-        firstName: 'Albion',
-        lastName: 'Paqarizi',
-        email: 'albion@example.com',
-      },
-      promotionId: 1,
-      promotion: {
-        id: 1,
-        name: 'Black-friday',
-        discount: 10,
-      },
     },
-    // Duhet te rregullohet nga fillimi
+    {
+      orderId: '2',
+      orderDate: '2023-06-02',
+      shippingDate: '2023-06-06',
+      orderFinalPrice: 80.0,
+      paymentStatus: 'Paid',
+      userId: '3',
+    },
+    {
+      orderId: '3',
+      orderDate: '2023-06-03',
+      shippingDate: '2023-06-07',
+      orderFinalPrice: 95.0,
+      paymentStatus: 'Pending',
+      userId: '4',
+    },
   ]);
 
   const handleDeleteOrder = (index) => {
@@ -61,39 +50,26 @@ const OrderData = () => {
             <NavLink to="/contact" activeClassName="active">Contact</NavLink>
           </li>
           <li>
-            <NavLink to="/admin/dbentities" activeClassName="active"> Db Entities</NavLink>
+            <NavLink to="/admin/dbentities" activeClassName="active">Db Entities</NavLink>
           </li>
         </ul>
       </nav>
       <div className="container">
         <h1>Order Data</h1>
+        <div className="add">
+          <a href="./orderdata" className="button">
+            Create Order
+          </a>
+        </div>
         <table>
           <thead>
             <tr>
               <th>Order ID</th>
               <th>Order Date</th>
               <th>Shipping Date</th>
-              <th>Order Price</th>
               <th>Order Final Price</th>
-              <th>Tracking ID</th>
-              <th>Carrier</th>
-              <th>Order Status</th>
               <th>Payment Status</th>
-              <th>Transaction ID</th>
-              <th>Payment Method ID</th>
-              <th>Payment Date</th>
-              <th>Payment Due Date</th>
-              <th>Phone Number</th>
-              <th>Street Address</th>
-              <th>City</th>
-              <th>Country</th>
-              <th>Postal Code</th>
-              <th>Name</th>
               <th>User ID</th>
-              <th>User Email</th>
-              <th>Promotion ID</th>
-              <th>Promotion Name</th>
-              <th>Promotion Discount</th>
               <th className="change">Change</th>
             </tr>
           </thead>
@@ -103,30 +79,12 @@ const OrderData = () => {
                 <td>{order.orderId}</td>
                 <td>{order.orderDate}</td>
                 <td>{order.shippingDate}</td>
-                <td>{order.orderPrice}</td>
                 <td>{order.orderFinalPrice}</td>
-                <td>{order.trackingId}</td>
-                <td>{order.carrier}</td>
-                <td>{order.orderStatus}</td>
                 <td>{order.paymentStatus}</td>
-                <td>{order.transactionId}</td>
-                <td>{order.paymentMethodId}</td>
-                <td>{order.paymentDate}</td>
-                <td>{order.paymentDueDate}</td>
-                <td>{order.phoneNumber}</td>
-                <td>{order.streetAddress}</td>
-                <td>{order.city}</td>
-                <td>{order.country}</td>
-                <td>{order.postalCode}</td>
-                <td>{order.name}</td>
                 <td>{order.userId}</td>
-                <td>{order.user.email}</td>
-                <td>{order.promotionId}</td>
-                <td>{order.promotion.name}</td>
-                <td>{order.promotion.discount}</td>
                 <td>
                   <button className="edit__button btn">Edit</button>
-                  <button className="delete__button btn" onClick={() => handleDeleteOrder(index)} >Delete</button>
+                  <button className="delete__button btn" onClick={() => handleDeleteOrder(index)}>Delete</button>
                 </td>
               </tr>
             ))}
--- bajonline/src/Pages/OurStory/OurStory.js
+++ bajonline/src/Pages/OurStory/OurStory.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import Navbar from '../Components/Navbar/Navbar'
-import Footer from '../Components/Footer/Footer'
-import "./OurStory.css"
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
+import './OurStory.css';
+
 const OurStory = () => {
-    return (
-        <>
-            <Navbar />
-            <div className='our__story'>
+  return (
+    <>
+      <Navbar />
+      <div className="our-story-container">
+        <div className="background-color">
+          <div className="overlay"></div>
+          <h1 className="our-story-title">Our Story</h1>
+        </div>
+        <div className="our-story-content">
+          {/* Add your story content here */}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-                <div className='title'>afdsadfs</div>
-                OurStory
-            </div>
-            <Footer/>
-        </>
-    )
-}
-
-export default OurStory
+export default OurStory;
